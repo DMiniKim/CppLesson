d26543151ee6d3eeec7d11c404bf12317cf5c356
DMiniKim <angks1212@naver.com>
2025-06-17 03:01:05 +0900

    (2025.06.17) 추상 클래스_과제

--- Program/Program/Vector2D.cpp
+++ Program/Program/Vector2D.cpp
@@ -12,5 +12,147 @@ Vector2D::Vector2D(int x , int y)
 
 Vector2D Vector2D::operator+(const Vector2D& ref)
 {
-	return Vector2D(this->x + ref.x, this->y + ref.y);
+	return Vector2D(this->x + ref.x, this->y + ref.y); // 각각 더하기
+}
+
+Vector2D Vector2D::operator-(const Vector2D& ref)
+{
+	return Vector2D(this->x - ref.x, this->y - ref.y);	// 각각 빼기
+}
+
+Vector2D Vector2D::operator*(float scale) const
+{
+	return Vector2D(this->x* scale, this->y * scale);	// 각각 곱하기
+}
+
+Vector2D Vector2D::operator/(float scale) const
+{
+	return Vector2D(this->x / scale, this->y / scale);	// 각각 나누기
+}
+
+Vector2D Vector2D::operator==(const Vector2D& ref) const
+{
+	Vector2D temp = {0,0};
+	if (this->x == ref.x && this->y == ref.y)			// 피연산자와 피연산자의 x와 y 값이 같을 경우 1을 대입하고
+	{													// 이외의 경우 전부 x와 y값에 0을 대입;
+		temp.x = 1;
+		temp.y = 1;
+	}
+	else
+	{
+		temp.x = 0;
+		temp.y = 0;
+	}
+
+	return temp;
+}
+
+Vector2D Vector2D::operator!=(const Vector2D& ref) const
+{
+	Vector2D temp = { 0,0 };
+	if (this->x != ref.x && this->y != ref.y)			// 피연산자와 피연산자의 x와 y 값이 다를 경우 1을 대입하고
+	{													// 이외의 경우 전부 x와 y값에 0을 대입;
+		temp.x = 1;
+		temp.y = 1;
+	}
+	else
+	{
+		temp.x = 0;
+		temp.y = 0;
+	}
+
+	return temp;
+}
+
+Vector2D Vector2D::operator<(const Vector2D& ref) const
+{
+	Vector2D temp = { 0,0 };							//  ****이렇게 할 수 있다를 설명하는 것임.****
+	if (this->x < ref.x && this->y < ref.y)				// 피연산자와 피연산자의 x와 y 값이 둘다 클 경우 1을 대입하고
+	{													// 이외의 경우 전부 x와 y값에 0을 대입;
+		temp.x = 1;										
+		temp.y = 1;
+	}
+	else
+	{
+		temp.x = 0;
+		temp.y = 0;
+	}
+
+	return temp;
+}
+
+Vector2D Vector2D::operator=(const Vector2D& ref)
+{
+	if (this != &ref)							// 코딩 실수로 자기 자신을 대입하는 경우도 있을 테니
+	{											// 그것에 대한 대비책
+		x = ref.x;
+		y = ref.y;
+	}
+	return *this;								// 어차피 나를 반환 하면 되니 *this;
+}
+
+Vector2D Vector2D::operator()(int temp) const
+{
+	x / temp;									// 람다 처럼 객체를 함수처럼 동작하게 하고 싶을 때
+	y + temp;
+	return *this;
+}
+
+Vector2D Vector2D::operator+() const
+{
+	return Vector2D(+x,+y); // 양수 전환
+}
+
+Vector2D Vector2D::operator-() const
+{
+	return Vector2D(-x,-y); // 음수 전환
+}
+
+Vector2D& Vector2D::operator++()
+{
+	++x;	
+	++y;
+	return *this; // 전위 연산의 경우는 자기자신을 계속 반환해서 연속으로 연산가능 하게한다.
+}
+
+Vector2D Vector2D::operator++(int)
+{
+	Vector2D temp = *this;
+	x++;
+	y++;
+	return temp; // 후위 연산의 경우는 변경전의 값을 반환해야함으로 값을 저장하고 증가시켜야함.
+}
+
+Vector2D& Vector2D::operator--()
+{
+	--x;
+	--y;
+	return *this; // 위와 동일
+}
+
+Vector2D Vector2D::operator--(int)
+{
+	Vector2D temp = *this;
+	x--;
+	y--;
+	return temp; // 위와 동일
+}
+
+Vector2D Vector2D::operator!() const
+{
+	// 헤더 파일에 왜 구현 안하는 지 설명 완료.
+	return ;
+}
+
+Vector2D Vector2D::operator~()
+{
+	return Vector2D(~x,~y); // 현재 Vector2D의 멤버 변수는 int 이므로 비트연산이 가능
+}
+
+Vector2D operator*(float temp, const Vector2D& ref)
+{
+	// 왼쪽 피연산자가 Vector 타입이 아니기에 friend 를 사용.
+
+	return Vector2D(ref.x * temp, ref.y * temp); // 하고싶은 연산 아무거나 연산시켜서 리턴
+													  
 }
--- Program/Program/Vector2D.h
+++ Program/Program/Vector2D.h
@@ -6,9 +6,44 @@ class Vector2D
 protected:
 	int x;
 	int y;
+	int arr[2];
 public:
 	Vector2D(int x, int y) ;
 	void ShowNum() const { cout << "x 값은 : " << x << endl; cout << "y 값은 : " << y << endl; }
+
+	// 단항 연산자를 활용한 operator
+	Vector2D operator+() const;		
+	Vector2D operator-() const;
+
+	Vector2D& operator++ ();
+	Vector2D operator++ (int);	// 후위 연산자임을 구분하기 위한 더미 파라미터
+	Vector2D& operator-- ();
+	Vector2D operator-- (int);	// 후위 연산자임을 구분하기 위한 더미 파라미터
+	
+	Vector2D operator! () const;// 보통 vector2d 같은 클래스 타입말고 bool 타입으로 진행되어야 옳지만
+								// 굳이 하자면 Vector방향을 뒤집는 형태로 쓰겠지만
+								// 위에 operator - 즉 단항 음수 연산자로 가능하기에 안 쓰임.
+	
+	Vector2D operator~ ();		// 이건 vector2D의 멤버 데이터가 int라 가능
+								// float의 경우는 비트 연산이 의미가 없어서 거의 안 쓰임.
+
+	// 이항 연산자를 활용한 operator
 	Vector2D operator+(const Vector2D& ref);
+	Vector2D operator-(const Vector2D& ref);
+	Vector2D operator*(float scale) const;
+	Vector2D operator/(float scale) const;
+	Vector2D operator==(const Vector2D& ref) const; // 이것도 위 ! 단항 연산자와 마찬가지로 vector2d 클래스 타입이 아닌
+													// bool이 옳은 표현이지만 굳이 표현 하면 
+													// true일 때 x = 1, y=1 이런식으로 가능은 하다.
+	Vector2D operator!=(const Vector2D& ref) const; // 이것도 마찬가지
+													
+	Vector2D operator<(const Vector2D& ref) const;	// 이것도 마찬가지 함수 내부에서 벡터크기 같은 걸 비교하고
+													// 클 경우 true 해서 x= 1, y= 1 이렇게 가능			
+	Vector2D operator=(const Vector2D& ref);
+	Vector2D operator()(int temp) const;
+	friend Vector2D operator* (float Scalar, const Vector2D& ref); // 위에 사용된 +나 -나 /나 %나 friend 로
+																   // 좌측 피연산자가 일반일 때 연산을 성립시킬 수 있다
+
 };
+
 
