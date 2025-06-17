9cee2911b24fad279abaa1ae09bd73d7470fb536
DMiniKim <angks1212@naver.com>
2025-06-17 15:44:11 +0900

    (2025.6.17) 템플릿

---
+++ Program/Class 06th ( Abstract )/Source.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include "../Program/Pencil.h"
+#include "../Program/Brush.h"
+#include "../Program/Vector2D.h"
+
+using namespace std;
+
+int main()
+{
+#pragma region 추상 클래스
+	// 함수에 특정한 내용이 구현되어 있지 않고, 선언만
+	// 되어있는 클래스입니다.
+
+	//	Paint* ptr = nullptr;
+	//	int num = 0;
+	//	while (1)
+	//	{
+	//		cin >> num;
+	//		switch (num)
+	//		{	
+	//		case 0: if (ptr == nullptr) {ptr = new Pencil;}
+	//			break;
+	//		case 1: if (ptr == nullptr) { ptr = new Brush; }
+	//			break;
+	//		default:
+	//			break;
+	//		}
+	//		if (ptr != nullptr)
+	//		{
+	//			ptr->Draw();
+	//			delete ptr;
+	//			ptr = nullptr;
+	//		}
+	//	}
+
+	// 추상 클래스는 정의되어 있지 않은 함수가 있으므로,
+	// 객체를 생성할 수 없습니다.
+#pragma endregion
+
+#pragma region 연산자 오버로딩 Overoading
+	Vector2D point1(3, 4);
+	Vector2D point2(1, 2);
+
+	Vector2D direction = point1 + point2;
+	direction.ShowNum();
+#pragma endregion
+
+#pragma region 단항 연산자
+	// 피연산자가 1개인 연산자
+	// 즉, 연산을 진행하는 대상이 하나 인 경우를 말한다.
+
+	// 혼잣말 하듯 자기 자신만 변화시키는 연산자
+
+	//ex) 코드 예시
+	//	int x = 0;
+	//	x++;
+	//	--x;
+	//	-x;
+	//	+x;
+	//	!x;
+	//	~x;
+
+	// 이렇게 연산을 진행하는 대상 자체가 하나일 경우
+	// 단항 연산자라고 한다.
+#pragma endregion
+
+#pragma region 이항 연산자
+	// 피연산자가 2개인 연산자
+	// 즉, 연산을 진행하는 대상이 2개 인 경우를 말한다.
+	// 니돈 + 내돈  = 우리돈 <<<---- 이렇게
+
+	//ex) 코드 예시
+	//	int a = 0;
+	//	int b = 1;
+	//	a + b;
+	//	a - b;
+	//	a * b;
+	//	a / b;
+	//	a % b;
+	//	a ^ b;
+	// 등등....
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,89 +1,88 @@
 #include <iostream>
-#include "../Program/Pencil.h"
-#include "../Program/Brush.h"
-#include "Vector2D.h"
+#include <Windows.h>
 
 using namespace std;
 
+template <typename T>
+bool Same(T x, T y)
+{
+	//bool isSame = x == y;
+	cout << "x 입력 값 : " << x << endl;
+	cout << "y 입력 값 : " << y << endl;
+	cout << "참일 시 1 ,거짓일 시 0 과연 => " << (x == y) << endl;
+	return x == y;
+}
+template <> 
+bool Same(const char* left, const char* right)
+{
+	return strlen(left) == strlen(right);
+}
+
+
+#define SIZE 5
+
+template<typename T>
+
+class Container
+{
+private:
+	T temp[SIZE];
+	int count = 0;
+public:
+	Container()
+	{
+		for (auto i = 0; i < SIZE; i++)
+		{
+			temp[i] = NULL;
+		}
+	}
+	T& operator[] (int data) // 해당 인덱스로 접근해서 출력용.
+	{
+		return this->temp[data];
+	}
+	void Add(T num) 
+	{
+		if (count < SIZE)
+		{
+			if (temp[count] == NULL)
+			{
+				temp[count++] = num;
+			}
+		}
+		else
+		{
+			cout << "꽊 차서 못 넣어요.." << endl;
+			return;
+		}
+	}
+	
+};
+
 int main()
 {
-#pragma region 추상 클래스
-	// 함수에 특정한 내용이 구현되어 있지 않고, 선언만
-	// 되어있는 클래스입니다.
+#pragma region 템플릿 Template
+	// 데이터 형식에 의존하지 않고 , 하나의 값이 여러 다른 데이터
+	// 형식을 가진 기술에 중점을 두어 재사용성을 높일 수 있는 기능.
+	// Same(4, 4);
+	// Same(1.2, 1.1);
+	// Same('A', 'a');
+	// Same("Dong Min", "GeumSoo");
 	
-	//	Paint* ptr = nullptr;
-	//	int num = 0;
-	//	while (1)
-	//	{
-	//		cin >> num;
-	//		switch (num)
-	//		{	
-	//		case 0: if (ptr == nullptr) {ptr = new Pencil;}
-	//			break;
-	//		case 1: if (ptr == nullptr) { ptr = new Brush; }
-	//			break;
-	//		default:
-	//			break;
-	//		}
-	//		if (ptr != nullptr)
-	//		{
-	//			ptr->Draw();
-	//			delete ptr;
-	//			ptr = nullptr;
-	//		}
-	//	}
-
-	// 추상 클래스는 정의되어 있지 않은 함수가 있으므로,
-	// 객체를 생성할 수 없습니다.
-#pragma endregion
-
-#pragma region 연산자 오버로딩 Overoading
-	Vector2D point1(3, 4);
-	Vector2D point2(1, 2);
 	
-	Vector2D direction = point1 + point2;
-	direction.ShowNum();
+	Container<int> temp;
+	temp.Add(54);
+	cout << temp[0] << endl;
+
+
 #pragma endregion
-
-#pragma region 단항 연산자
-	// 피연산자가 1개인 연산자
-	// 즉, 연산을 진행하는 대상이 하나 인 경우를 말한다.
-
-	// 혼잣말 하듯 자기 자신만 변화시키는 연산자
 	
-	//ex) 코드 예시
-	int x = 0;
-	x++;
-	--x;
-	-x;
-	+x;
-	!x;
-	~x;
-
-	// 이렇게 연산을 진행하는 대상 자체가 하나일 경우
-	// 단항 연산자라고 한다.
+#pragma region 템플릿의 특수화
+	// 특정 자료형에 대해 다르게 처리하고 싶은 경우
+	// 특정한 자료형만 다른 형식으로 동작시키는 기능.
+	// cout << Same("DongMin", "GeumSoo") << endl;
 #pragma endregion
-
-#pragma region 이항 연산자
-	// 피연산자가 2개인 연산자
-	// 즉, 연산을 진행하는 대상이 2개 인 경우를 말한다.
-	// 니돈 + 내돈  = 우리돈 <<<---- 이렇게
-
-	//ex) 코드 예시
-	int a = 0;
-	int b = 1;
-	a + b;
-	a - b;
-	a * b;
-	a / b;
-	a % b;
-	a ^ b;
-	// 등등....
-#pragma endregion
+	
 
 
-	//숙제 -> 단항 연산자 , 이항 연산자
-
-	// 다양한 연산자 만들어보기
 	return 0;
 }
